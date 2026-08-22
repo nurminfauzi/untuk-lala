50207dd3ce3d8910d3803cfe305c33e2c2123b44
nurminfauzi <74889214+nurminfauzi@users.noreply.github.com>
2026-08-22 18:26:21 +0700

    love letter 2.js

--- love letter 2.js
+++ love letter 2.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
   var envelope = $("#envelope");
   var btn_open = $("#open");
   var btn_reset = $("#reset");
+  
+  // Tangkap elemen audio dari HTML
+  var music = document.getElementById("bg-music");
 
   envelope.click(function () {
     open();
@@ -15,8 +18,15 @@ $(document).ready(function () {
 
   function open() {
     envelope.addClass("open").removeClass("close");
+    // Mainkan musik saat amplop dibuka
+    music.play();
   }
+  
   function close() {
     envelope.addClass("close").removeClass("open");
+    // Hentikan musik saat amplop ditutup
+    music.pause();
+    // Kembalikan durasi musik ke detik 0 (awal)
+    music.currentTime = 0;
   }
 });
